195499e236b8924c81dbc804daa0b757319e32b5
Timothy-olumide1 <olabodetimothyolumide@gmail.com>
2023-09-08 14:33:15 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Task 3
+ * Description - lowercase and uppercase
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+        char afabets;
+
+        for (afabets = 'a'; afabets <= 'z'; afabets++)
+                putchar(afabets);
+        for (afabets = 'A'; afabets <= 'Z'; afabets++)
+                putchar(afabets);
+        putchar('\n');
+
+        return (0);
+}
