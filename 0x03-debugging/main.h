50311e63a16eb4ecc82614ddcb821d9b47d25f30
Timothy-olumide1 <olabodetimothyolumide@gmail.com>
2023-09-14 08:10:26 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,3 +6,23 @@
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
+
+int largest_number(int a, int b, int c)
+{
+    int largest;
+
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
