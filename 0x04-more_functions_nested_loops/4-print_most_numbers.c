b1c12616f005aeff66ab88bfd6cf44ffcb334521
Timothy-olumide1 <olabodetimothyolumide@gmail.com>
2023-09-14 14:22:58 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+		_putchar(i);
+		}
+
+	}
+	_putchar('\n');
+}
