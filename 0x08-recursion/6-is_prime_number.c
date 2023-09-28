12b2c9d02f70dc4d7231ae20fbcc0e15f874814f
Timothy-olumide1 <olabodetimothyolumide@gmail.com>
2023-09-28 16:04:24 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * is_prime_number2 - returns 1 if the input integer is a prime number
+ * @n: first byte of the memory
+ * @i: first byte of the memory
+ * Return: -
+ */
+
+int is_prime_number2(int n, int i)
+{
+	if (i > n / 2)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime_number2(n, i + 2));
+}
+
+/**
+ * is_prime_number - fills memory with a constant byte.
+ * @n: first bytes of the memory
+ * Return: -
+ */
+int is_prime_number(int n)
+{
+	int i = 3;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	if (n % 2 == 0)
+	{
+		return (0);
+	}
+	return (is_prime_number2(n, i));
+}
